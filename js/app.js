487cee74c920d55f2317115f4d326d68788b7b2a
Omar Paredes <omare.paredes@gmail.com>
2019-06-20 00:26:53 -0500

    Agregamos estilos para menú

--- js/app.js
+++ js/app.js
@@ -9,3 +9,14 @@ if ( navigator.serviceWorker ) {
 
     navigator.serviceWorker.register(swLocation);
 }
+
+$(document).ready(function(){
+
+    var header = $('header'),
+                btn    = $('button.toggle-nav');
+
+    btn.on('click', function(){
+          header.toggleClass('active');
+    });
+
+});
